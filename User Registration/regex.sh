16b5e0c7915561073deeeaab553aa8216e889686
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-09-04 00:54:21 +0530

    added code to check the email given is valid or not using regex

--- User Registration/regex.sh
+++ User Registration/regex.sh
@@ -6,7 +6,11 @@ read firstName
 echo "Enter the LastName "
 read lastName
 
+echo "Enter the Email "
+read email
+
 regex=^[A-Z][a-z]{1,}[^0-9]$
+emailRegex=^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]$
 
 if [[ $firstName =~ $regex ]]
 then
@@ -21,3 +25,11 @@ then
 else
 	echo "Invalid LastName"
 fi
+
+if [[ $email =~ $emailRegex ]]
+then
+	echo "Valid Email"
+else
+	echo "Invalid Email"
+fi
+
